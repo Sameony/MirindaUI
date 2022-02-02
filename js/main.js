ec145d5f5196e862b4e221f79e632c2140354dbf
Sameony <iam2001sam@gmail.com>
2022-02-02 21:03:06 +0530

    feat: added Grid layout

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const closedIco = document.querySelector(".closed-ico");
 const openIco = document.querySelector(".open-ico");
 const titles = document.querySelectorAll('.smol-title');
 const drawer = document.querySelector(".drawer");
+const grid = document.querySelector(".grid");
+
 
 openIco.addEventListener("click", ()=>
 {
@@ -14,6 +16,7 @@ openIco.addEventListener("click", ()=>
     {
         titles[i].style.display="none";
     }
+    grid.style.gridTemplateColumns="[col-1]5rem [col-2]1fr [col3]";;
 })
 closedIco.addEventListener("click", ()=>
 {
@@ -25,4 +28,5 @@ closedIco.addEventListener("click", ()=>
     {
         titles[i].style.display="block";
     }
+    grid.style.gridTemplateColumns="[col-1]18.75rem [col-2]1fr [col3]";
 })
