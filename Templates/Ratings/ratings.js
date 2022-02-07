7d021242fa6445318ec7b959519ae65c9959492d
Sameony <iam2001sam@gmail.com>
2022-02-07 21:44:56 +0530

    feat: added functionality to the basic ratings

---
+++ Templates/Ratings/ratings.js
@@ -0,0 +1,31 @@
+const stars = document.querySelectorAll(".star");
+const hearts = document.querySelectorAll(".heart");
+
+stars.forEach(star=>star.addEventListener("click", ratingChange(stars)))
+hearts.forEach(heart=>heart.addEventListener("click", ratingChange(hearts)))
+
+function ratingChange(rate)
+{
+    for (let i = 0; i < rate.length; i++) {
+        let j=0;
+        while(j+5 <i)
+        {
+            j+=5;
+        }
+        rate[i].addEventListener("click", () => {
+            for (; j <=i; j++) {
+                if (rate[j].classList.contains("fas") === false) 
+                {
+                    rate[j].classList.add("fas");
+                }
+            }
+            for (let k=i+1; k <rate.length && k%5!=0; k++) {
+                if (rate[k].classList.contains("fas")) 
+                {
+                    rate[k].classList.remove("fas");
+                }
+            }
+        })
+    
+    }
+}
