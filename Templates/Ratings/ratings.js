0a5749b5b7335a82c4ad77713fc40b1cdd9a4398
Sameony <iam2001sam@gmail.com>
2022-02-08 13:18:59 +0530

    feat: calls changed to callbacks

--- Templates/Ratings/ratings.js
+++ Templates/Ratings/ratings.js
@@ -1,8 +1,8 @@
 const stars = document.querySelectorAll(".star");
 const hearts = document.querySelectorAll(".heart");
 
-stars.forEach(star => star.addEventListener("click", ratingChange(stars)))
-hearts.forEach(heart => heart.addEventListener("click", ratingChange(hearts)))
+stars.forEach(star => star.addEventListener("click", ()=> ratingChange(stars)))
+hearts.forEach(heart => heart.addEventListener("click", ()=> ratingChange(hearts)))
 
 function ratingChange(rate) {
     for (let i = 0; i < rate.length; i++) {
